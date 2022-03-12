05e012bca11577ba675486689d9532e0b10696db
codejay411 <jaypr202@gmail.com>
2022-03-13 01:15:28 +0530

    update code

--- videoconferencing/settings.py
+++ videoconferencing/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['videoconferencing321.herokuapp.com', 'localhost', '127.0.0.1:8000', '*']
 
